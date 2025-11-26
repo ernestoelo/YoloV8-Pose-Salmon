be5a549b19626653062e89d783dcc85790f33ef4
tobarcaceres <tobarcaceres@gmail.com>
2025-11-26 14:14:40 -0300

    Clean up repository: Keep only YoloV8_Pose_Fine_Tuning and rename best run

--- YoloV11_Pose/src/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# src/models/__init__.py
-from .yolo_wrapper import YOLOv8PoseTrainer
-
-__all__ = ['YOLOv8PoseTrainer']
--- YoloV11_Pose/src/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-# src/utils/__init__.py
-from .download_utils import ModelDownloader
-
-__all__ = ['ModelDownloader']
