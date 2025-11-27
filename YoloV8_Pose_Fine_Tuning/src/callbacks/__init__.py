2b1f080060be596f5e2d22a67aceb49895766f2a
ernestoelo <ernesto.gamero@sansano.usm.cl>
2025-11-27 17:38:26 -0300

    Delete YoloV8_Pose_Fine_Tuning folder

--- YoloV8_Pose_Fine_Tuning/src/callbacks/__init__.py
+++
@@ -1,3 +0,0 @@
-from .custom_metrics_callback import CustomMetricsCallback
-
-__all__ = ['CustomMetricsCallback']
--- YoloV8_Pose_Fine_Tuning/src/metrics/__init__.py
+++
@@ -1,5 +0,0 @@
-from .pck import PCKMetric
-from .oks import OKSMetric
-from .evaluator import PoseEvaluator
-
-__all__ = ['PCKMetric', 'OKSMetric', 'PoseEvaluator']
